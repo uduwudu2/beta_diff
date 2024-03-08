c7e5152cc6b0b478e1d0b49f9ec235790528b3da
hajime_kuno <hajimekuno10@gmail.com>
2024-03-09 02:42:39 +0900

    fix integrate

--- beta_diff/beta_diff.py
+++ beta_diff/beta_diff.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import integrate
-from scipy.special import beta
+from scipy.special import betaln
 
 class beta_diff:
     def __init__(self, alpha1, beta1, alpha2, beta2):
@@ -10,55 +10,58 @@ class beta_diff:
         self.beta2 = beta2
 
     @staticmethod
-    def jnt_dst(s, t, alpha1, beta1, alpha2, beta2):
-        return (
-            (s + t) ** (alpha1 - 1)
-            * (1 - s - t) ** (beta1 - 1)
-            * s ** (alpha2 - 1)
-            * (1 - s) ** (beta2 - 1)
-        )
+    def jnt_dst(s, t, alpha1, beta1, alpha2, beta2, betaln1, betaln2):
+        val = (alpha1 - 1) * np.log(s+t) + (beta1 - 1) * np.log(1 - s - t) + (alpha2 - 1) * np.log(s) + (beta2 - 1) * np.log(1 - s) - betaln1 - betaln2
+        return np.exp(val)
 
     def pdf(self, t):
+        betaln1 = betaln(self.alpha1, self.beta1)
+        betaln2 = betaln(self.alpha2, self.beta2)
         if t <= -1 or t >= 1:
             return 0
         elif t <= 0:
-            return integrate.quad(
+            val = integrate.quad(
                 beta_diff.jnt_dst,
                 -t,
                 1,
-                args=(t, self.alpha1, self.beta1, self.alpha2, self.beta2),
-            )[0] / (beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2))
+                args=(t, self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
+            )[0]
+            return val
         else:
-            return integrate.quad(
+            val = integrate.quad(
                 beta_diff.jnt_dst,
                 0,
                 1 - t,
-                args=(t, self.alpha1, self.beta1, self.alpha2, self.beta2),
-            )[0] / (beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2))
+                args=(t, self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
+            )[0]
+            return val
 
     def cdf(self, t):
+        betaln1 = betaln(self.alpha1, self.beta1)
+        betaln2 = betaln(self.alpha2, self.beta2)
         if t <= -1:
             return 0
         elif t >= 1:
             return 1
         elif t <= 0:
-            return integrate.dblquad(
+            val = integrate.dblquad(
                 beta_diff.jnt_dst,
                 -1,
                 t,
                 lambda x: -x,
                 1,
-                args=(self.alpha1, self.beta1, self.alpha2, self.beta2),
-            )[0] / (beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2))
+                args=(self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
+            )[0]
+            return val
         else:
-            return (
+            val = (
                 integrate.dblquad(
                     beta_diff.jnt_dst,
                     -1,
                     0,
                     lambda x: -x,
                     1,
-                    args=(self.alpha1, self.beta1, self.alpha2, self.beta2),
+                    args=(self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
                 )[0]
                 + integrate.dblquad(
                     beta_diff.jnt_dst,
@@ -66,28 +69,26 @@ class beta_diff:
                     t,
                     0,
                     lambda x: 1 - x,
-                    args=(self.alpha1, self.beta1, self.alpha2, self.beta2),
+                    args=(self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
                 )[0]
-            ) / (beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2))
+            )
+            return val
 
     @staticmethod
-    def jnt_dst_for_mean(s, t, alpha1, beta1, alpha2, beta2):
-        return (
-            t
-            * (s + t) ** (alpha1 - 1)
-            * (1 - s - t) ** (beta1 - 1)
-            * s ** (alpha2 - 1)
-            * (1 - s) ** (beta2 - 1)
-        )
+    def jnt_dst_for_mean(s, t, alpha1, beta1, alpha2, beta2, betaln1, betaln2):
+        val = (alpha1 - 1) * np.log(s+t) + (beta1 - 1) * np.log(1 - s - t) + (alpha2 - 1) * np.log(s) + (beta2 - 1) * np.log(1 - s) - betaln1 - betaln2
+        return t * beta_diff.jnt_dst(s, t, alpha1, beta1, alpha2, beta2, betaln1, betaln2)
 
     def mean(self):
+        betaln1 = betaln(self.alpha1, self.beta1)
+        betaln2 = betaln(self.alpha2, self.beta2)
         mean = integrate.dblquad(
             beta_diff.jnt_dst_for_mean,
             -1,
             0,
             lambda x: -x,
             1,
-            args=(self.alpha1, self.beta1, self.alpha2, self.beta2),
+            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
         )[0]
         mean += integrate.dblquad(
             beta_diff.jnt_dst_for_mean,
@@ -95,30 +96,25 @@ class beta_diff:
             1,
             0,
             lambda x: 1 - x,
-            args=(self.alpha1, self.beta1, self.alpha2, self.beta2),
+            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, betaln1, betaln2),
         )[0]
-        mean /= beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2)
         return mean
 
     @staticmethod
-    def jnt_dst_for_var(s, t, alpha1, beta1, alpha2, beta2, mean):
-        return (
-            (t - mean) ** 2
-            * (s + t) ** (alpha1 - 1)
-            * (1 - s - t) ** (beta1 - 1)
-            * s ** (alpha2 - 1)
-            * (1 - s) ** (beta2 - 1)
-        )
+    def jnt_dst_for_var(s, t, alpha1, beta1, alpha2, beta2, mean, betaln1, betaln2):
+        return (t-mean) ** 2 * beta_diff.jnt_dst(s, t, alpha1, beta1, alpha2, beta2, betaln1, betaln2)
 
     def var(self):
         mean = self.mean()
+        betaln1 = betaln(self.alpha1, self.beta1)
+        betaln2 = betaln(self.alpha2, self.beta2)
         var = integrate.dblquad(
             beta_diff.jnt_dst_for_var,
             -1,
             0,
             lambda x: -x,
             1,
-            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, mean),
+            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, mean, betaln1, betaln2),
         )[0]
         var += integrate.dblquad(
             beta_diff.jnt_dst_for_var,
@@ -126,9 +122,8 @@ class beta_diff:
             1,
             0,
             lambda x: 1 - x,
-            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, mean),
+            args=(self.alpha1, self.beta1, self.alpha2, self.beta2, mean, betaln1, betaln2),
         )[0]
-        var /= beta(self.alpha1, self.beta1) * beta(self.alpha2, self.beta2)
         return var
 
     def std(self):
